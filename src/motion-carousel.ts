e1847e37a77049600343dcb543979225871f7755
liuyue <1308972436@qq.com>
2023-09-06 23:58:12 +0800

    feat: 切换元素

--- src/motion-carousel.ts
+++ src/motion-carousel.ts
@@ -1,6 +1,7 @@
 import type { PropertyValueMap } from 'lit'
 import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { customElement, property, query } from 'lit/decorators.js'
+import { styleMap } from 'lit/directives/style-map.js'
 
 @customElement('motion-carousel')
 class MotionCarousel extends LitElement {
@@ -12,7 +13,6 @@ class MotionCarousel extends LitElement {
             width:800px;
             height:533px;
             border-radius:8px;
-            background-color:red;
             cursor: pointer;
             box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
         }
@@ -24,50 +24,65 @@ class MotionCarousel extends LitElement {
         }
         @unocss-placeholder
     `
+
   @property({ type: Number })
-  curIndex = 0
+  curIndex = 1
+
+  @query('slot[name="cur"]', true)
+  curSlots!: HTMLSlotElement
+
+  @query('slot[name="prev"]', true)
+  prevSlots!: HTMLSlotElement
 
   get maxIndex() {
-    return this.childElementCount - 1
+    return this.children.length - 1
   }
+
+  private prevIndex = 0
 
   hasValidIndex() {
     return this.curIndex >= 0 && this.curIndex <= this.maxIndex
   }
 
-  private clickHandler(e: MouseEvent) {
-    const i = this.curIndex + (Number(!e.shiftKey) || -1)
-    this.curIndex = i > this.maxIndex ? 0 : i < 0 ? this.maxIndex : i
-    const change = new CustomEvent('change',
-      { detail: this.curIndex, bubbles: true, composed: true })
-    this.dispatchEvent(change)
+  handleClick(e: MouseEvent) {
+    this.curIndex = this.curIndex + (e.shiftKey ? -1 : 1)
+    this.curIndex = this.curIndex >= this.maxIndex ? this.maxIndex : this.curIndex <= 0 ? 0 : this.curIndex
+    // lit中如果是由于用户交互行为导致propert改变，需要手动dispatch一个event出去
+    const event = new CustomEvent('change', { bubbles: true, composed: true, detail: this.curIndex })
+    this.dispatchEvent(event)
   }
 
-  render() {
-    if (this.hasValidIndex()) {
-      //
-    }
-    return html`
-            <div class="w-100% h-100% pos-relative" @click=${this.clickHandler}>
-                <slot name="selected"></slot>
-            </div>
-        `
-  }
-
-  private prevIndex = 0
-
+  // 在元素更新完毕后调用，可以获取到更新后的dom1的元素
   protected updated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
-    console.log('_changedProperties', _changedProperties)
     if (_changedProperties.has('curIndex') && this.hasValidIndex()) {
-      this.updatedSlots()
+      this.updateSlots()
       this.prevIndex = this.curIndex
     }
   }
 
-  private updatedSlots() {
-    console.log('this.children', this.children, this.prevIndex, this.curIndex)
-    this.children[this.prevIndex].removeAttribute('slot')
-    this.children[this.curIndex].setAttribute('slot', 'selected')
+  updateSlots() {
+    /* 先把插槽内的内容清空 */
+    this.curSlots.assignedElements()[0]?.removeAttribute('slot')
+    this.prevSlots.assignedElements()[0]?.removeAttribute('slot')
+    /* 根据现在的index显示对应的图片 */
+    this.children[this.prevIndex].setAttribute('slot', 'previous')
+    this.children[this.curIndex].setAttribute('slot', 'cur')
+  }
+
+  render() {
+    const animateLeft = ''
+    const curLeft = ''
+    const prevLeft = ''
+    return html`
+      <div class="w-100% h-100% pos-relative shadow" @click=${this.handleClick} style=${styleMap({ left: animateLeft })}>
+          <div class="w-100% h-100% pos-relative" style=${styleMap({ left: curLeft })}>
+             <slot name="prev"></slot>
+          </div>
+          <div class="w-100% h-100% pos-relative top--100%" style=${styleMap({ left: prevLeft })}>
+             <slot name="cur"></slot>
+          </div>
+      </div>
+    `
   }
 }
 
